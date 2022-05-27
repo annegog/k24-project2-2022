59ad8ea8aaa9d0d2b9f246780b648ec45e8b0b98
annegog <gogoulaanna@gmail.com>
2022-05-27 14:59:38 +0300

    dataServeer

--- dataServer.c
+++ dataServer.c
@@ -12,49 +12,93 @@
 #include <pthread.h>         /* For threads */
 #include <string.h>
 
-#include "ADTVector.h"
+#include "ADTDeque.h"
+#include "ADTDeque.c"
 #include "pool.h"
+
+#define BUFF 4096
 
 #define perror2(s,e) fprintf(stderr, "%s: %s\n", s, strerror(e))
 
+/***************************************************************************************/
+
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cvar;                                             /* Condition variable */
+
+/***************************************************************************************/
+
 void perror_exit(char *message);
 void sigchld_handler (int sig);
+    
+/***************************************************************************************/
 
-//vector_create(10,NULL);
+void *communication_thread(void *argp){ /* Thread function */
+    printf("I am the newly created communication thread %ld\n", pthread_self());
+    int sock = *(int*)argp;
+    char buff[BUFF];
 
-void read_what_the_client_send(){
+    if(read(sock, buff, BUFF) < 0){
+        perror_exit("read");
+    }   
+    printf("I read: %s\n", buff);
 
+    // να διβάσει το path που εστειλε ο client
+   
+    //deque_insert_first(deque, a);
+    // να βάλει το αρχελιο στην ουρά
+    // αν η ουρά ειναι γεμάτη περιμένει
+
+    free(argp);
+    pthread_exit(NULL);
 }
 
-void push_the_file_in_the_queue(){
+/***************************************************************************************/
 
+void *worker_thread(void *arg){
+    int err;
+
+    printf("Just created a worker thread %ld\n", pthread_self());
+    //printf("Thread %ld: Waiting for signal\n", pthread_self());
+    pthread_cond_wait(&cvar, &mtx); //* Wait for signal
+
+    if (err = pthread_mutex_lock(&mtx)) {            /* Lock mutex */
+        perror2("pthread_mutex_lock", err); exit(1); }
+    printf("AAA Thread %ld: Locked the mutex\n", pthread_self());
+
+    // META
+    // gia na to bgalw apo to wait
+    /* Wake up one thread waiting for condition variable COND.  */
+    pthread_cond_signal(&cvar);                          /* Awake thread */
+    printf("Thread %ld: Sent signal\n", pthread_self());
+
+    if (err = pthread_mutex_unlock(&mtx)) {                  /* Unlock mutex */
+        perror2("pthread_mutex_unlock", err); exit(1); }
+    printf("Thread %ld: Unlocked the mutex\n", pthread_self());
+
+    pthread_exit(NULL);
 }
 
-
-void *communication_thread(void *argp){ /* Thread function */ 
-    printf("I am the newly created communication-thread %ld\n", pthread_self());
-    // ????????????
-
-    read_what_the_client_send();
-    push_the_file_in_the_queue();
-}
-
-pthread_mutex_t mtx;
+/***************************************************************************************/
 
 
 int main(int argc, char *argv[]){
     
     int port, sock, newsock;
     int thread_pool_size=0, queue_size=0, block_size=0;
+    
     struct sockaddr_in server, client;
     socklen_t clientlen;
     struct sockaddr *serverptr=(struct sockaddr *)&server;
     struct sockaddr *clientptr=(struct sockaddr *)&client;
     struct hostent *rem;
     
-    pthread_t thr;
+    pthread_t thr_com, thr_work;
     int err, status;
-  
+
+    pthread_cond_init(&cvar, NULL); // initialize condition variable
+    
+    /******************************** arguments ********************************************/
+    
     if (argc != 9){
         // ./dataServer -p <port> -s <thread_pool_size> -q <queue_size> -b <block_size>
         printf("Worng arguments\n");
@@ -79,39 +123,85 @@ int main(int argc, char *argv[]){
             printf("block size: %d\n", block_size);
         }
     }
+
     // Reap dead children asynchronously
     signal(SIGCHLD, sigchld_handler);
 
+    // create the deque of files
+    Deque queue_of_files = deque_create(queue_size,NULL);
+
+    /************************** create the worker threads **********************************/
+    
+    // make a pool- array for the worker threads
+    pthread_t * pool_threads_array;
+    pool_threads_array = malloc (sizeof(pthread_t) * thread_pool_size);
+    
+    for(pthread_t i=0; i<thread_pool_size; i++){
+        // Lock mutex
+        if (err = pthread_mutex_lock(&mtx)){                           
+            perror2("pthread_mutex_lock", err); exit(1); }
+        //printf("Thread %ld: Locked the mutex\n", pthread_self());
+
+        if (err = pthread_create(&thr_work, NULL, worker_thread, NULL)){
+            perror2("pthread_create", err);
+            exit(1);
+        }
+        printf("Thread %ld: Created thread %ld\n", pthread_self(), thr_work);
+
+        pool_threads_array[i]= thr_work;
+
+        // Unlock mutex
+        if (pthread_mutex_unlock(&mtx)) {                     
+            perror("pthread_mutex_unlock"); exit(1); }
+        //printf("Thread %ld: Unlocked the mutex\n", pthread_self());
+    }
+
+    /***************************************************************************************/
+
     // Create socket
+    //printf("create socket\n");
     if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
         perror_exit("socket");
     server.sin_family = AF_INET;       /* Internet domain */
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons(port);      /* The given port */
+
     /* Bind socket to address */
+    //printf("Bind socket to address\n");
     if (bind(sock, serverptr, sizeof(server)) < 0)
         perror_exit("bind");
     /* Listen for connections */
-    if (listen(sock, 200) < 0) perror_exit("listen");
+    if (listen(sock, 200) < 0) 
+        perror_exit("listen");
     printf("Listening for connections to port %d\n", port);
     while(1){ 
         clientlen = sizeof(client);
-        // accept connection
     	if ((newsock = accept(sock, clientptr, &clientlen)) < 0) 
             perror_exit("accept");
     	printf("Accepted connection from localhost\n");
-    	// making the communication thread
-        // if (err = pthread_create(&thr, NULL, communication_thread, NULL)) { /* New thread */
-        //     perror2("pthread_create", err);
-        //     exit(1);
-        // }
-
-
-
-
+        
+        /************************* making the communication thread *************************/
+        
+        int* pr_new = malloc(sizeof(int));
+        *pr_new= newsock;
+        if (err = pthread_create(&thr_com, NULL, communication_thread, pr_new)){
+            perror2("pthread_create", err);
+            exit(1);
+        }
     	//close(newsock); //------- parent closes socket to client
     }
+    
+    /***************************************************************************************/
+    
+    // delete the com-thread
+    if (err = pthread_join(thr_com, (void **) &status)) { /* Wait for thread */
+        perror2("pthread_join", err); /* termination */
+        exit(1);
+    }
+    printf("Thread %ld exited with code %d\n", thr_com, status);
+    pthread_exit(NULL);
 
+    free(pool_threads_array);
     return 0;
 }
 
