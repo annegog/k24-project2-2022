6e6ca12f8f16f88d083e805da0d4e5023daf056f
annegog <gogoulaanna@gmail.com>
2022-05-31 19:03:24 +0300

    client

---
+++ client_/client.h
@@ -0,0 +1,93 @@
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+#include <stdio.h>
+#include <sys/types.h>	     /* sockets */
+#include <sys/socket.h>	     /* sockets */
+#include <netinet/in.h>	     /* internet sockets */
+#include <unistd.h>          /* read, write, close */
+#include <netdb.h>	         /* gethostbyaddr */
+#include <stdlib.h>	         /* exit */
+#include <string.h>	         /* strlen */
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#define BUFF 4096
+
+/***************************************************************************************/
+
+int find_the_file(char* dir, char* file){
+    int fd;
+    DIR *folder;
+    struct dirent *entry;
+    char new_folder[BUFF];
+    folder = opendir(dir);
+    // if the is no
+    if(folder == NULL){
+        perror("Unable to read directory");
+    }
+    else{
+        fd = mkdir(dir,S_IRWXU);
+        folder = opendir(dir);
+        if(folder == NULL){
+            perror("Unable to read directory");
+        }
+    }
+    if( (entry=readdir(folder))){
+        if (entry->d_type == DT_DIR){
+            if( !(strcmp(entry->d_name,".") == 0 || strcmp(entry->d_name,"..") == 0) ){
+                sprintf(new_folder, "%s/%s", dir, entry->d_name );
+                find_the_file(new_folder, file);
+            }
+        }
+        if (entry->d_type == DT_REG){
+            if( entry->d_name == file){
+                if (remove(file) == 0) {
+                    printf("The file is deleted successfully.");
+                } 
+                else {
+                    printf("The file is not deleted.");
+                }
+            }
+        }
+    }
+    return fd;
+}
+
+/***********************************************************************/
+
+int read_data (int fd, char *buffer){/* Read formated data */
+	char temp;int i = 0, length = 0;
+	if ( read ( fd, &temp, 1 ) < 0 )	/* Get length of string */
+		exit (-3);
+	length = temp;
+	while ( i < length )	/* Read $length chars */
+		if ( i < ( i+= read (fd, &buffer[i], length - i)))
+			exit (-3);
+	return i;	/* Return size of string */
+}
+
+/*************************************************************/
+
+char* separate(char* path){
+    char s[2] = "/";
+    char* temp;
+    char* file = path;
+    temp = strtok(path, s);
+    while( temp != NULL ){
+        strcpy(file, temp);
+        temp = strtok(NULL, s);
+    }
+    return file;
+}
+
+/*************************************************************/
+
+void perror_exit(char *message){
+    perror(message);
+    exit(EXIT_FAILURE);
+}
+
+#endif
--- client_/remoteClient.c
+++ client_/remoteClient.c
@@ -7,18 +7,19 @@
 #include <netdb.h>	         /* gethostbyaddr */
 #include <stdlib.h>	         /* exit */
 #include <string.h>	         /* strlen */
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
-#define BUFF 4096
+#include "client.h"
 
-void perror_exit(char *message);
+int main(int argc, char *argv[]){
 
-
-main(int argc, char *argv[]){
-
-    int port, sock, i;
-    char buf[BUFF];
+    int port, sock;
+    char buffer[BUFSIZ];
+    char buffer_read[BUFSIZ];
     char *directory;
-
     struct sockaddr_in server;
     struct sockaddr *serverptr = (struct sockaddr*)&server;
     struct hostent *rem;
@@ -57,24 +58,43 @@ main(int argc, char *argv[]){
     /* Initiate connection */
     if (connect(sock, serverptr, sizeof(server)) < 0)
 	   perror_exit("connect");
-    printf("Connecting to %s port %d\n", rem->h_name, port);
+    printf("Connecting to %s port %d\n\n", rem->h_name, port);
 
-    strcpy(buf,directory);
-    while (1){
-    
-        // write the path we want to copy on client's file system
-        if (write(sock,buf, BUFF) < 0)
-            perror_exit("write");
-    
+    // write the path we want to copy on client's file system
+    if (write(sock, directory, strlen(directory)+1) < 0)
+        perror_exit("write");
 
+    while(1){
+
+        read_data(sock,buffer);
+        
+        char* file = separate(buffer);
+        printf("File: %s\n",file);
+        
+        /*********************************************************/
+        int fd = find_the_file(directory,file);
+
+        // create a file in the current dir.
+        // so we copy the (server) file to the /file        
+        FILE *write_file = fopen(file, "w");
+        if ( write_file == NULL){   
+            printf("Error! Could not open file\n"); 
+            exit(EXIT_FAILURE); 
+        }
+
+        // while( recv(sock, buffer_read, BUFF, 0) > 0){
+        //     //printf(">>THE DATA:\n%s //telos\n", buffer_read);
+        //     fprintf(write_file,"%s", buffer_read);
+        //     sleep(1);
+        // }
+        // usleep(10000);
+        // printf("out of the while recv\n");
+
+        fprintf(write_file,"skata...out of the while!");
+        
+        fclose(write_file);
+        close(fd);
     }
 
-
-
-}
-
-
-void perror_exit(char *message){
-    perror(message);
-    exit(EXIT_FAILURE);
+    return 0;
 }
